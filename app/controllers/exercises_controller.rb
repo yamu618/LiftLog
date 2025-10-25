f4477ac76461f61b4c79dccd84412d68d54e41c9
yamu <shoshuto0618@gmail.com>
2025-10-25 16:58:28 +0900

    exercisesコントローラー設計

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,49 +1,88 @@
 class ExercisesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_exercise, only: [:update, :destroy]
+  before_action :set_exercise, only: %i[edit update destroy]
 
   def index
     @categories = Category.all
-    @selected_category = params[:category_id].present? ? Category.find(params[:category_id]) : nil
-
-    if @selected_category
-      @exercises = current_user.exercises.where(category: @selected_category)
-    else
-      @exercises = current_user.exercises
-    end
+    @selected_category = params[:category_id].present? ? Category.find(params[:category_id]) : @categories.first
+    @exercises = current_user.exercises.where(category: @selected_category)
 
     respond_to do |format|
-      format.html
       format.turbo_stream
+      format.html
     end
   end
 
   def create
-    @exercise = current_user.exercises.new(exercise_params)
-    if @exercise.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to exercises_path, notice: "種目を追加しました" }
-      end
-    else
-      render turbo_stream: turbo_stream.replace(
-        "exercise_form",
-        partial: "form",
-        locals: { exercise: @exercise }
-      )
+  @exercise = current_user.exercises.new(exercise_params)
+  
+  if @exercise.save
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: [
+          turbo_stream.append("exercises_list", partial: "exercise", locals: { exercise: @exercise }),
+          turbo_stream.replace("exercise_form", partial: "form", locals: { exercise: Exercise.new })
+        ]
+      }
+      format.html { redirect_to exercises_path, notice: "種目を追加しました" }
+    end
+  else
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.replace(
+          "exercise_form",
+          partial: "form",
+          locals: { exercise: @exercise }
+        )
+      }
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
+end
+
+  def edit
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.replace(
+          "exercise_#{@exercise.id}",
+          partial: "form",
+          locals: { exercise: @exercise }
+        )
+      }
+      format.html
     end
   end
 
   def update
-    @exercise.update(exercise_params)
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to exercises_path }
+    if @exercise.update(exercise_params)
+      respond_to do |format|
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace(
+            "exercise_#{@exercise.id}",
+            partial: "exercise",
+            locals: { exercise: @exercise }
+          )
+        }
+        format.html { redirect_to exercises_path, notice: "種目を更新しました"}
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace(
+            "exercise_#{@exercise.id}",
+            partial: "form",
+            locals: { exercise: @exercise }
+          )
+        }
+        format.html { render :edit, status: :unprocessable_entity}
+      end
     end
   end
 
+
   def destroy
     @exercise.destroy
+
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to exercises_path }

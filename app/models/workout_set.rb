04051a0c0951632820bd8f7360515c6b09731335
yamu <shoshuto0618@gmail.com>
2025-11-06 12:34:00 +0900

    workut_setsテーブルにdurationとdistanceカラムを追加

--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -3,6 +3,8 @@
 # Table name: workout_sets
 #
 #  id         :bigint           not null, primary key
+#  distance   :float            default(0.0), not null
+#  duration   :integer          default(0), not null
 #  reps       :integer          default(0), not null
 #  weight     :float            default(0.0), not null
 #  created_at :datetime         not null

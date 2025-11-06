6b3afc3aa8a25004343dae3e6ffb7bdade1b1f61
yamu <shoshuto0618@gmail.com>
2025-11-06 17:15:59 +0900

    利用規約とプライバシーポリシー用のコントローラ―作成、root設定

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def terms; end
+
+  def privacy; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 
   get "home/index"
 
+  get "/terms", to: "pages#terms", as: :terms
+  get "/privacy", to: "pages#privacy", as: :privacy
+
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest

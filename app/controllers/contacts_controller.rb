f3b9b09bcfa2ca6c4a41d25893d10e60c48d36a6
yamu <shoshuto0618@gmail.com>
2025-11-06 19:04:20 +0900

    お問い合わせ機能のテストを作成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def new
     @contact = Contact.new
   end
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
       flash[:notice] = "お問い合わせを送信しました"
       redirect_to new_contact_path
     else
-      flash.now[:alert] = "送信に失敗しました。もう一度お試しください"
+      flash.now[:alert] = @contact.errors.full_messages.join(" / ")
       render :new, status: :unprocessable_entity
     end
   end

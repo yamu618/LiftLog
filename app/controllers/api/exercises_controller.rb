d7d47eead7df3dcfa7cb60d20e8a76c5e1645e89
yamu <shoshuto0618@gmail.com>
2025-11-21 20:04:36 +0900

    カタカナ変換

--- app/controllers/api/exercises_controller.rb
+++ app/controllers/api/exercises_controller.rb
@@ -1,14 +1,17 @@
 class Api::ExercisesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    query = params[:query]
+    query = params[:query].to_s
     category_id = params[:category_id]
 
-    # queryが空の場合、何も返さない
     return render json: [] if query.blank?
+
+    query_kana = query.tr('ぁ-ん', 'ァ-ン')
 
     exercises = current_user.exercises
                             .where(category_id: category_id)
-                            .where("name LIKE ?", "#{query}%")
+                            .where("name LIKE ?", "#{query_kana}%")
                             .order(:name)
                             .limit(10)
 

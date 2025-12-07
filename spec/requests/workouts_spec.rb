a7f7dff32862c37ea6cc47f51f1143ae2e2a2a9b
yamu <shoshuto0618@gmail.com>
2025-12-07 14:23:01 +0900

    リクエストテスト削除

--- spec/requests/workouts_spec.rb
+++ spec/requests/workouts_spec.rb
@@ -1,7 +1,5 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe "Workouts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  
 end

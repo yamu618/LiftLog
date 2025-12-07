ebdcdd3b5eb72f04ac049f33f5d1400d595a59fc
yamu <shoshuto0618@gmail.com>
2025-12-07 20:38:48 +0900

    deploy用コミット

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "ユーザー認証", type: :system do
     expect(page).to have_content("アカウント登録が完了しました")
   end
 
-  xit "ユーザーがログイン出来る" do
+  it "ユーザーがログイン出来る" do
     user = create(:user)
     visit new_user_session_path
 

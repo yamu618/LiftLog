e3057443f7f1cfa680f297a7d8be61e8cd54e08e
yamu <shoshuto0618@gmail.com>
2025-12-07 14:21:38 +0900

    modelテストの作成

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -11,8 +11,31 @@
 #
 #  index_categories_on_name  (name) UNIQUE
 #
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it 'nameがあれば有効' do
+      category = build(:category)
+      expect(category).to be_valid
+    end
+
+    it 'nameがなければ無効' do
+      category = build(:category, name: nil)
+      expect(category).not_to be_valid
+    end
+
+    it 'nameが重複していれば無効' do
+      create(:category, name: 'テスト')
+      dup = build(:category, name: 'テスト')
+      expect(dup).not_to be_valid
+    end
+  end
+
+  describe 'アソシエーション' do
+    it 'exercisesを持つことができる' do
+      assoc = described_class.reflect_on_association(:exercises)
+      expect(assoc.macro).to eq :has_many
+    end
+  end
 end
--- spec/models/workout_spec.rb
+++ spec/models/workout_spec.rb
@@ -23,8 +23,55 @@
 #  fk_rails_...  (exercise_id => exercises.id)
 #  fk_rails_...  (user_id => users.id)
 #
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Workout, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーション' do
+    it 'user に属している' do
+      assoc = described_class.reflect_on_association(:user)
+      expect(assoc.macro).to eq :belongs_to
+    end
+
+    it 'exercise に属している' do
+      assoc = described_class.reflect_on_association(:exercise)
+      expect(assoc.macro).to eq :belongs_to
+    end
+
+    it 'workout_sets を複数持つ' do
+      assoc = described_class.reflect_on_association(:workout_sets)
+      expect(assoc.macro).to eq :has_many
+    end
+  end
+
+  describe 'バリデーション' do
+    it 'performed_on が必須' do
+      workout = build(:workout, performed_on: nil)
+      expect(workout).not_to be_valid
+    end
+  end
+
+  describe 'インスタンスメソッド' do
+    it 'total_weight が合計を返す' do
+      workout = create(:workout)
+      create(:workout_set, workout:, weight: 10, reps: 5)
+
+      expect(workout.total_weight).to eq(50)
+    end
+
+    it 'total_distance の合計が返る' do
+      workout = create(:workout)
+      create(:workout_set, workout:, distance: 300)
+      create(:workout_set, workout:, distance: 200)
+
+      expect(workout.total_distance).to eq(500)
+    end
+
+    it 'total_duration の合計が返る' do
+      workout = create(:workout)
+      create(:workout_set, workout:, duration: 60)
+      create(:workout_set, workout:, duration: 30)
+
+      expect(workout.total_duration).to eq(90)
+    end
+  end
 end

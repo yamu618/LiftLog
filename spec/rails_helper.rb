38c6a573d4ee9c772c91fccbdf6b25aaba890c63
yamu <shoshuto0618@gmail.com>
2025-12-07 16:04:51 +0900

    githubaction用

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,16 +38,11 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system) do
     driven_by :remote_chrome
-    
-    #if ENV['GITHUB_ACTIONS']
-    #  Capybara.server_host = '0.0.0.0'
-    #  Capybara.server_port = 3000
-    #  Capybara.app_host = "http://127.0.0.1:3000" 
-    #else
-    #  Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    #  Capybara.server_port = 4444
-    #  Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-    #end
+
+    Capybara.server_host = '0.0.0.0'
+    Capybara.server_port = 3000
+    Capybara.app_host = "http://host.docker.internal:3000"
+    Capybara.default_max_wait_time = 15
 
     Capybara.ignore_hidden_elements = false
   end

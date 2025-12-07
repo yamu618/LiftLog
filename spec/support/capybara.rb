02e5e4e1db0ef9395f39fa2e3ccfa4c3e4ab09e5
yamu <shoshuto0618@gmail.com>
2025-12-07 16:34:07 +0900

    webdriver gem削除

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,7 +11,7 @@ end
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("--no-sandbox")
-  options.add_argument("--headless")
+  options.add_argument("--headless=new")
   options.add_argument("--disable-gpu")
   options.add_argument("--disable-dev-shm-usage")
   options.add_argument("--window-size=1680,1050")

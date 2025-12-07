8b7c0662ef848ad052849d7bf53e52dc39a8039f
yamu <shoshuto0618@gmail.com>
2025-12-07 15:34:13 +0900

    githubaction用

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,22 +37,20 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system) do
+    driven_by :remote_chrome
+    
     if ENV['GITHUB_ACTIONS']
-      options = Selenium::WebDriver::Chrome::Options.new
-      options.add_argument("--headless")
-      options.add_argument("--disable-gpu")
-      options.add_argument("--no-sandbox")
-      options.add_argument("--disable-dev-shm-usage")
-      options.add_argument("--window-size=1400,1400")
-
-      driven_by :selenium, using: :chrome, screen_size: [1400,1400], options: options
+      driven_by :selenium_chrome_headless, screen_size: [1680, 1050]
+      Capybara.server_host = '0.0.0.0'
+      Capybara.server_port = 3000
+      Capybara.app_host = "http://localhost:3000"
     else
-      driven_by :remote_chrome
       Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
       Capybara.server_port = 4444
       Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-      Capybara.ignore_hidden_elements = false
     end
+
+    Capybara.ignore_hidden_elements = false
   end
 
   config.include ActionView::RecordIdentifier, type: :system
